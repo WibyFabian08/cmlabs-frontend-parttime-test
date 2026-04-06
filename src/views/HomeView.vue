63b51f9399966bcc0829aa2cada9dbd4f78059c1
Wiby Fabian <wibyfabian08@gmail.com>
2026-04-06 15:03:59 +0700

    test endpoint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+const listData = ref([]);
+
+onMounted(() => {
+  axios
+    .get(
+      "https://www.themealdb.com/api/json/v1/1/list.php?i=list&limit=10&page=1",
+    )
+    .then((res) => {
+      console.log(res.data);
+
+      listData.value = res.data?.meals;
+    });
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div v-for="(data, index) in listData" :key="index">{{ data?.strIngredient }}</div>
 </template>
