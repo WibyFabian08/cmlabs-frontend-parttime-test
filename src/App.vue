fa6b9af319e055c28e2126945d55889b99c43ead
Wiby Fabian <wibyfabian08@gmail.com>
2026-04-06 16:28:59 +0700

    component navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { RouterView } from "vue-router";
+
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
-  <RouterView />
+  <NavBar />
+
+  
+  <div class="px-10 pt-5">
+    <RouterView />
+  </div>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<script setup>
+const MENUS = ["Home", "Foods", "Ingredients", "Local Culinary"];
+</script>
+
+<template>
+  <nav
+    class="h-12 flex items-center px-10 justify-between border-b border-gray-200"
+  >
+    <h6 class="text-xl font-semibold">MealApp</h6>
+
+    <ul class="flex items-center gap-5">
+      <li v-for="menu in MENUS" :key="menu">
+        <RouterLink class="hover:underline text-sm font-normal">
+          {{ menu }}
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
