4f38832c6d8f1f009630d1d2cb8cf790e10cbd79
Wiby Fabian <wibyfabian08@gmail.com>
2026-04-06 16:35:49 +0700

    hero component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 import { RouterView } from "vue-router";
 
 import NavBar from "@/components/NavBar.vue";
+import Hero from "@/components/Hero.vue";
 </script>
 
 <template>
   <NavBar />
+  <Hero/>
 
-  
   <div class="px-10 pt-5">
     <RouterView />
   </div>
---
+++ src/components/Hero.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="flex items-center flex-col gap-1 justify-center h-80 bg-gray-100">
+    <p class="text-sm">😀 😊 😁</p>
+    <p class="text-sm text-black">MealApp Websiter</p>
+    <h3 class="text-3xl text-black font-bold text-center">
+      See All The Delicious Foods
+    </h3>
+  </div>
+</template>
